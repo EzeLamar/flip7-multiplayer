d45704c744dc19830b52ef30f0f007dbc4cb4394
EzeLamar <ezequiel.lamarquee@gmail.com>
2024-11-22 14:27:30 -0300

    Add Buy me a coffee button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+import { BuyMeACoffeeButton } from "@/components/BuyMeACoffeeButton";
 import { GameLobby } from "@/components/game-lobby";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
       <div className="container mx-auto px-4 py-8">
-        <div className="mb-8 flex flex-col items-center gap-2">
-        <h1 className="text-4xl font-bold text-white text-center">
-          FLIP7 Online
-        </h1>
-        <a className="text-white hover:text-gray-300" href="https://github.com/EzeLamar" target="_blank">Developed by Ezequiel Lamarque</a>
+        <div className="mb-5 flex flex-col items-center gap-2">
+          <h1 className="text-4xl font-bold text-white text-center">
+            FLIP7 Online
+          </h1>
+          <a
+            className="text-white hover:text-gray-300"
+            href="https://github.com/EzeLamar"
+            target="_blank"
+          >
+            Developed by Ezequiel Lamarque
+          </a>
+          <BuyMeACoffeeButton />
         </div>
         <GameLobby />
       </div>
---
+++ components/BuyMeACoffeeButton.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export function BuyMeACoffeeButton() {
+  return (
+    <a href="https://www.buymeacoffee.com/ezelamar" target="_blank">
+      <Image
+        className="hover:opacity-90"
+        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+        alt="Buy Me A Coffee"
+        width={150}
+        height={35}
+      />
+    </a>
+  );
+}
